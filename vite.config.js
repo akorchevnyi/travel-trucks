fb887960677c05736a9a66684ec420a7bf152e21
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2025-01-18 21:29:49 +0200

    fix: change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { fileURLToPath } from "url";
-import path from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-    plugins: [react()],
-    assetsInclude: ['**/*.svg'],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src")
-        }
-    }
+    plugins      : [react()],
+    assetsInclude: ["**/*.svg"]
 });
