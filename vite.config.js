196bdfc64b4885b29b546bb232cfd310f49919e2
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2025-01-18 22:49:31 +0200

    ready

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import { resolve, dirname } from "path";
-// import { fileURLToPath } from 'url';
-//
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
 
 export default defineConfig({
     base: './',
