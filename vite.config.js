e94ae48cec60a0a4631b3b5ed604706926ea5a2a
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2025-01-18 22:15:16 +0200

    fix: change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// import { resolve, dirname } from "path";
+// import { fileURLToPath } from 'url';
+//
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 export default defineConfig({
     base: './',
     plugins      : [react()],
     assetsInclude: ["**/*.svg"],
     build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                sprite: resolve(__dirname, 'src/assets/sprite.svg')
-            },
-        },
+        assetsInlineLimit: 0
     },
-
 });
