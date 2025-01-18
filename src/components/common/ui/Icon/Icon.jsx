7bb50d3cb785c6c2cbb01307eeedadb2815c1172
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2025-01-18 21:43:07 +0200

    fix: change vite.config

--- src/components/common/ui/Icon/Icon.jsx
+++ src/components/common/ui/Icon/Icon.jsx
@@ -8,7 +8,7 @@ export default function Icon({ onClick = null, width, height, styles, type }) {
 
     return (
         <svg onClick={onClick} className={iconStyle} width={width} height={height}>
-            <use xlinkHref={`/src/assets/sprite.svg#icon-${type}`}></use>
+            <use href={`/src/assets/sprite.svg#icon-${type}`}></use>
         </svg>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
+    base: './',
     plugins      : [react()],
-    assetsInclude: ["**/*.svg"]
+    assetsInclude: ["**/*.svg"],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                sprite: resolve(__dirname, 'src/assets/sprite.svg')
+            },
+        },
+    },
+
 });
